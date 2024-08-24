6453e4f7c323bafcd99bfffce9f42fb5272ed6f1
tcocco20 <theo@coccoscoding.dev>
2024-08-24 13:21:37 -0700

    started customizing color pallette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors, { neutral, orange, zinc } from "tailwindcss/colors";
 
 const config: Config = {
   content: [
@@ -7,6 +8,37 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: {
+        50: "#fff1f1",
+        100: "#ffdfdf",
+        200: "#ffc5c5",
+        300: "#ff9d9d",
+        400: "#ff6465",
+        500: "#ff2d2e",
+        600: "#ed1516",
+        700: "#c80d0e",
+        800: "#a50f10",
+        900: "#881415",
+        950: "#4b0404",
+      },
+      orange: colors.orange,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+      slate: colors.slate,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
