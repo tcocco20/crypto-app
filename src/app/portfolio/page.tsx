087091961640c0d4e7e141d08faf9c77a82ce3be
tcocco20 <theo@coccoscoding.dev>
2024-08-27 13:20:35 -0700

    removed test component

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,10 +1,7 @@
-import TestComponent from "@/components/TestComponent";
-
 const Portfolio = () => {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-2xl">Portfolio Page</h1>
-      <TestComponent />
     </main>
   );
 };
