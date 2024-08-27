644140a1367dd3b1a8568bb85427e39647d91c5c
tcocco20 <theo@coccoscoding.dev>
2024-08-27 12:17:21 -0700

    created test page, made preferences slice, will make portfolio slice next then test both on multiple pages

---
+++ src/lib/features/preferences/preferencesSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import type { RootState } from "../../store";
+
+export interface PreferencesState {
+  selectedCurrency: string;
+  darkMode: boolean;
+}
+
+const initialState: PreferencesState = {
+  selectedCurrency: "USD",
+  darkMode: true,
+};
+
+export const preferencesSlice = createSlice({
+  name: "preferences",
+  initialState,
+  reducers: {
+    toggleDarkMode: (state) => {
+      state.darkMode = !state.darkMode;
+    },
+    setSelectedCurrency: (state, action: PayloadAction<string>) => {
+      state.selectedCurrency = action.payload;
+    },
+  },
+});
+
+export const { toggleDarkMode, setSelectedCurrency } = preferencesSlice.actions;
+
+// // Other code such as selectors can use the imported `RootState` type
+// export const selectCount = (state: RootState) => state.counter.value;
+
+export default preferencesSlice.reducer;
