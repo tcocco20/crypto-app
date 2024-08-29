a1bb5f6896eacf7105ba02e7f48a55bbafef89f4
tcocco20 <theo@coccoscoding.dev>
2024-08-29 09:30:07 -0700

    added logo and started styling buttons

--- src/components/MobileHeader.tsx/ActionButtons.tsx
+++ src/components/MobileHeader.tsx/ActionButtons.tsx
@@ -1,5 +1,22 @@
+"use client";
+import { useAppSelector } from "@/lib/hooks";
+import { Search, SunDim } from "lucide-react";
+
 const ActionButtons = () => {
-  return <div></div>;
+  const selectedCurrency = useAppSelector(
+    (state) => state.preferences.selectedCurrency
+  );
+  return (
+    <div className="flex gap-2">
+      <button className="p-2 rounded-sm text-white">
+        <Search />
+      </button>
+      <button className="p-2 rounded-sm text-white">{selectedCurrency}</button>
+      <button className="p-2 rounded-sm text-white">
+        <SunDim />
+      </button>
+    </div>
+  );
 };
 
 export default ActionButtons;
--- src/components/MobileHeader.tsx/HeaderActions.tsx
+++ src/components/MobileHeader.tsx/HeaderActions.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import ActionButtons from "./ActionButtons";
 
 const HeaderActions = () => {
   return (
-    <div>
-      <Image src="/images/mobile-logo.png" alt="Logo" width={40} height={40} />
+    <div className="p-4 flex justify-between items-center">
+      <div>
+        <Image
+          src="/images/mobile-logo.png"
+          alt="Logo"
+          width={36}
+          height={20}
+        />
+      </div>
+      <ActionButtons />
     </div>
   );
 };
