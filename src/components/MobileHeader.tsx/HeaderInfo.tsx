e4761284a39e05e6c9f65e0a071006194d331221
tcocco20 <theo@coccoscoding.dev>
2024-08-30 15:22:19 -0700

    still trying to style meter element

--- src/components/MobileHeader.tsx/HeaderInfo.tsx
+++ src/components/MobileHeader.tsx/HeaderInfo.tsx
@@ -3,7 +3,7 @@ import MarketCapMeter from "../UI/MarketCapMeter";
 
 const HeaderInfo = () => {
   return (
-    <div className="bg-indigo-800 flex justify-between px-1 py-4 text-white">
+    <div className="bg-indigo-900/80 flex justify-between p-4 border border-gray-600/80 text-white">
       <MarketCapMeter label="$124.45B" max={100} value={12} id="totalValue" />
       <MarketCapMeter label="44%" max={100} value={44} id="bitcoin" />
       <MarketCapMeter label="21%" max={100} value={21} id="ethereum" />
