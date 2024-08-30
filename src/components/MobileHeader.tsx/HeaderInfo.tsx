6fbfb233c9a518aa8d0d99f8d50a87a32eb8db7c
tcocco20 <theo@coccoscoding.dev>
2024-08-30 14:51:18 -0700

    started styling meters

--- src/components/MobileHeader.tsx/HeaderInfo.tsx
+++ src/components/MobileHeader.tsx/HeaderInfo.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import MarketCapMeter from "../UI/MarketCapMeter";
 
 const HeaderInfo = () => {
-  return <div className="bg-indigo-800">
-    
-  </div>;
+  return (
+    <div className="bg-indigo-800 flex justify-between px-1 py-4 text-white">
+      <MarketCapMeter label="$124.45B" max={100} value={12} id="totalValue" />
+      <MarketCapMeter label="44%" max={100} value={44} id="bitcoin" />
+      <MarketCapMeter label="21%" max={100} value={21} id="ethereum" />
+    </div>
+  );
 };
 
 export default HeaderInfo;
--- src/components/UI/MarketCapMeter.tsx
+++ src/components/UI/MarketCapMeter.tsx
@@ -1,21 +1,39 @@
-import React from "react";
+interface MeterProps {
+  id: string;
+  className?: string;
+  value: number;
+  max: number;
+  min?: number;
+}
 
-interface OneLabelMeter extends HTMLMeterElement {
+interface OneLabelMeter {
+  labelClasses?: string;
   label: string;
 }
 
-interface TwoLabelMeter extends HTMLMeterElement {
+interface TwoLabelMeter {
   startLabel: string;
   endLabel: string;
 }
 
-type MarketCapMeterProps = OneLabelMeter | TwoLabelMeter;
+type MarketCapMeterProps = MeterProps & (OneLabelMeter | TwoLabelMeter);
 
 const MarketCapMeter = (props: MarketCapMeterProps) => {
+  if ("startLabel" in props) {
+    return null;
+  }
+
   return (
-    <div>
-      <label htmlFor=""></label>
-      <meter value={props.value}></meter>
+    <div className="flex gap-2 items-center text-xs">
+      <label className={props.labelClasses} htmlFor={props.id}>
+        {props.label}
+      </label>
+      <meter
+        className={props.className}
+        id={props.id}
+        value={props.value}
+        max={props.max}
+      />
     </div>
   );
 };
