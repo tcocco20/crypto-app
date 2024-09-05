5afeb27282143a31bf82a19209ed55750f8f55ec
tcocco20 <theo@coccoscoding.dev>
2024-09-05 12:45:56 -0700

    made nav links reusable

--- src/components/MobileNavbar/NavLink.tsx
+++ src/components/MobileNavbar/NavLink.tsx
@@ -1,16 +1,32 @@
 import Link from "next/link";
-
 import Image from "next/image";
 
-const NavLink = () => {
+interface NavLinkProps {
+  href: string;
+  icon: string;
+  label: string;
+}
+
+const NavLink = ({ href, icon, label }: NavLinkProps) => {
+  //   const router = useRouter();
+  //   const { pathname } = router;
+  const active = false;
   return (
-    <div className="relative p-[1px] bg-gradient-to-b from-indigo-300 to-indigo-600 rounded-md">
+    <div
+      className={`${
+        active
+          ? "relative p-[1px] bg-gradient-to-b from-indigo-300 to-indigo-600 rounded-md shadow-2xl shadow-indigo-500"
+          : ""
+      }`}
+    >
       <Link
-        href="/"
-        className="flex flex-col justify-center items-center py-2 px-6 rounded-md bg-indigo-700/90"
+        href={href}
+        className={`flex flex-col justify-center items-center py-2 px-6 rounded-md ${
+          active && "bg-indigo-700/90"
+        }`}
       >
-        <Image src={"/icons/home.svg"} alt="" width={24} height={24} />
-        <p className="text-xs font-extralight text-white">Overview</p>
+        <Image src={icon} width={24} height={24} alt={`Link to ${label}`} />
+        <p className="text-xs font-extralight text-white">{label}</p>
       </Link>
     </div>
   );
