f34881b55cd209273ab08bb5f2713acb5f270d1b
tcocco20 <theo@coccoscoding.dev>
2024-09-05 12:10:04 -0700

    almost done styling nav links, will make reusable next

--- src/components/MobileNavbar/NavLink.tsx
+++ src/components/MobileNavbar/NavLink.tsx
@@ -1,7 +1,19 @@
-// import Link from "next/link";
+import Link from "next/link";
+
+import Image from "next/image";
 
 const NavLink = () => {
-  return <div>NavLink</div>;
+  return (
+    <div className="relative p-[1px] bg-gradient-to-b from-indigo-300 to-indigo-600 rounded-md">
+      <Link
+        href="/"
+        className="flex flex-col justify-center items-center py-2 px-6 rounded-md bg-indigo-700/90"
+      >
+        <Image src={"/icons/home.svg"} alt="" width={24} height={24} />
+        <p className="text-xs font-extralight text-white">Overview</p>
+      </Link>
+    </div>
+  );
 };
 
 export default NavLink;
