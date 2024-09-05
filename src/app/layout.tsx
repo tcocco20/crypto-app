4bfe90ea4698ec0dc35c834d409f0a8337595c2f
tcocco20 <theo@coccoscoding.dev>
2024-09-05 11:34:21 -0700

    added icons and created navlinks, added bg to app to see what it looks like with opacity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-gray-800`}>
         <StoreProvider>
           {children}
           <MobileNavbar />
--- src/components/MobileNavbar/index.tsx
+++ src/components/MobileNavbar/index.tsx
@@ -1,7 +1,11 @@
+import NavLink from "./NavLink";
+
 const MobileNavbar = () => {
   return (
-    <nav className="z-50 fixed bottom-0 left-0 border-t border-t-gray-600 w-full bg-gray-800">
-      MobileNavbar
+    <nav className="z-50 fixed bottom-0 left-0 border-t border-t-gray-600 w-full bg-indigo-950/30 backdrop-blur-sm p-3 flex justify-between">
+      <NavLink />
+      <NavLink />
+      <NavLink />
     </nav>
   );
 };
