49b356c5843daace2960c9fcc59e6bc6ddb95561
tcocco20 <theo@coccoscoding.dev>
2024-09-10 16:20:14 -0700

    started styling top bar

--- src/components/DesktopHeader/TopBar.tsx
+++ src/components/DesktopHeader/TopBar.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import MarketCapMeter from "../UI/MarketCapMeter";
+import config from "../../../tailwind.config";
 
 const TopBar = () => {
-  return <div>TopBar</div>;
+  let colors: any;
+  if (config.theme?.colors) {
+    colors = config.theme.colors;
+  }
+
+  return (
+    <div className="bg-indigo-900/50 flex gap-3 p-4 border-b border-gray-600/80 text-white justify-center">
+      <p className="text-xs">Coins 7884</p>
+      <p className="text-xs">Exchange 622</p>
+      <p className="text-xs">1.68 T</p>
+      <MarketCapMeter
+        label="$124.45B"
+        value={20}
+        max={100}
+        height="7px"
+        barContainerClassName="bg-gray-300/80"
+        color="#fff"
+      />
+      <MarketCapMeter
+        label="44%"
+        value={44}
+        max={100}
+        height="7px"
+        barContainerClassName="bg-gray-300/80"
+        color={colors.yellow[400]}
+      />
+      <MarketCapMeter
+        label="21%"
+        value={21}
+        max={100}
+        height="7px"
+        color={colors.indigo[400]}
+        barContainerClassName="bg-gray-300/80"
+      />
+    </div>
+  );
 };
 
 export default TopBar;
--- src/components/DesktopHeader/index.tsx
+++ src/components/DesktopHeader/index.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { useIsMobile } from "@/hooks/useIsMobile";
+import TopBar from "./TopBar";
 
 const DesktopHeader = () => {
   const isMobile = useIsMobile();
 
   if (isMobile) return null;
 
-  return <header>DesktopHeader</header>;
+  return (
+    <header>
+      <TopBar />
+    </header>
+  );
 };
 
 export default DesktopHeader;
