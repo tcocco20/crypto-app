24a09e9f1b400b543512b9b1e50b8220579c6d91
tcocco20 <theo@coccoscoding.dev>
2024-09-11 11:57:44 -0700

    finished most of styling, want to add icons to text component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-800 md:bg-gray-950`}>
+      <body className={`${inter.className} bg-gray-800 md:bg-gray-900`}>
         <StoreProvider>
           <MobileLayout />
           <DesktopHeader />
--- src/components/DesktopHeader/TopBar.tsx
+++ src/components/DesktopHeader/TopBar.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import MarketCapMeter from "../UI/MarketCapMeter";
 import config from "../../../tailwind.config";
+import TopBarText from "./TopBarText";
+import { Zap } from "lucide-react";
 
 const TopBar = () => {
   let colors: any;
@@ -9,34 +11,36 @@ const TopBar = () => {
   }
 
   return (
-    <div className="bg-indigo-900/50 flex gap-3 p-4 border-b border-gray-600/80 text-white justify-center">
-      <p className="text-xs">Coins 7884</p>
-      <p className="text-xs">Exchange 622</p>
-      <p className="text-xs">1.68 T</p>
-      <MarketCapMeter
-        label="$124.45B"
-        value={20}
-        max={100}
-        height="7px"
-        barContainerClassName="bg-gray-300/80"
-        color="#fff"
-      />
-      <MarketCapMeter
-        label="44%"
-        value={44}
-        max={100}
-        height="7px"
-        barContainerClassName="bg-gray-300/80"
-        color={colors.yellow[400]}
-      />
-      <MarketCapMeter
-        label="21%"
-        value={21}
-        max={100}
-        height="7px"
-        color={colors.indigo[400]}
-        barContainerClassName="bg-gray-300/80"
-      />
+    <div className="bg-indigo-900/40 p-4 border-b border-gray-600/80 text-white justify-center">
+      <div className="max-w-4xl mx-auto flex gap-11 justify-center text-sm">
+        <TopBarText text="Coins" value={7884} icon={<Zap />} />
+        <TopBarText text="Exchange" value={622} />
+        <TopBarText text="" value={"1.68 T"} />
+        <MarketCapMeter
+          label="$124.45B"
+          value={20}
+          max={100}
+          height="7px"
+          barContainerClassName="bg-gray-300/80"
+          color="#fff"
+        />
+        <MarketCapMeter
+          label="44%"
+          value={44}
+          max={100}
+          height="7px"
+          barContainerClassName="bg-gray-300/80"
+          color={colors.yellow[400]}
+        />
+        <MarketCapMeter
+          label="21%"
+          value={21}
+          max={100}
+          height="7px"
+          color={colors.indigo[400]}
+          barContainerClassName="bg-gray-300/80"
+        />
+      </div>
     </div>
   );
 };
