1f1fa9d0a5cebabf1d02d2fb8ae999039853d984
tcocco20 <theo@coccoscoding.dev>
2024-09-11 12:34:05 -0700

    finished basic top bar functionality

--- src/components/DesktopHeader/TopBar.tsx
+++ src/components/DesktopHeader/TopBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import MarketCapMeter from "../UI/MarketCapMeter";
 import config from "../../../tailwind.config";
 import TopBarText from "./TopBarText";
-import { Zap } from "lucide-react";
+import { ChevronUp, SendToBack, Zap } from "lucide-react";
 
 const TopBar = () => {
   let colors: any;
@@ -11,11 +11,29 @@ const TopBar = () => {
   }
 
   return (
-    <div className="bg-indigo-900/40 p-4 border-b border-gray-600/80 text-white justify-center">
+    <div className="bg-indigo-900/40 p-5 border-b border-gray-600/80 text-white justify-center">
       <div className="max-w-4xl mx-auto flex gap-11 justify-center text-sm">
-        <TopBarText text="Coins" value={7884} icon={<Zap />} />
-        <TopBarText text="Exchange" value={622} />
-        <TopBarText text="" value={"1.68 T"} />
+        <TopBarText
+          text="Coins"
+          value={7884}
+          icon={
+            <Zap
+              fill="#000"
+              color="#000"
+              size={12}
+              className="rounded-full bg-white w-4 h-4 p-1"
+            />
+          }
+        />
+        <TopBarText
+          text="Exchange"
+          value={622}
+          icon={<SendToBack fill="#fff" size={18} />}
+        />
+        <TopBarText
+          value={"1.68 T"}
+          icon={<ChevronUp className="text-cyan-500" size={16} />}
+        />
         <MarketCapMeter
           label="$124.45B"
           value={20}
--- src/components/DesktopHeader/TopBarText.tsx
+++ src/components/DesktopHeader/TopBarText.tsx
@@ -1,16 +1,16 @@
 import { type ReactNode } from "react";
 
 interface TopBarTextProps {
-  text: string;
+  text?: string;
   value: string | number;
   icon?: ReactNode;
 }
 
 const TopBarText = ({ text, value, icon }: TopBarTextProps) => {
   return (
-    <div>
+    <div className="flex gap-2 items-center">
       {!!icon && icon}
-      <span className="text-gray-400">{text} </span>
+      {!!text && <span className="text-gray-400">{text}</span>}
       <span className="text-white">{value}</span>
     </div>
   );
