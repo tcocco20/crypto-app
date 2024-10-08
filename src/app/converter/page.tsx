de6a5ca62f8eb77ad454a08c39f38f6da62dff09
tcocco20 <theo@coccoscoding.dev>
2024-10-08 15:02:11 -0700

    added title for converter page

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -2,8 +2,16 @@ import HomeNav from "@/components/homeComponents/HomeNav";
 
 const Converter = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-3 md:px-24">
       <HomeNav />
+      <div className="text-start my-5 self-start">
+        <h2 className="dark:text-white text-lg mb-1">
+          Online Currency Converter
+        </h2>
+        <p className="text-gray-800 dark:text-gray-400 font-light text-sm">
+          09/29/2023 14:15
+        </p>
+      </div>
     </main>
   );
 };
