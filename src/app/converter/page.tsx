8e27beeb188e335f7875ef2ad9e6f1614a4ba43f
tcocco20 <theo@coccoscoding.dev>
2024-10-09 09:47:45 -0700

    fixed the page a bit, set up folder for converter container, planning structure for component

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -1,17 +1,19 @@
+import ConverterContainer from "@/components/ConverterContainer";
 import HomeNav from "@/components/homeComponents/HomeNav";
 
 const Converter = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-3 md:px-24">
+    <main className="flex min-h-screen flex-col items-center px-3 md:px-24">
       <HomeNav />
       <div className="text-start my-5 self-start">
-        <h2 className="dark:text-white text-lg mb-1">
+        <h1 className="dark:text-white text-lg mb-1">
           Online Currency Converter
-        </h2>
+        </h1>
         <p className="text-gray-800 dark:text-gray-400 font-light text-sm">
           09/29/2023 14:15
         </p>
       </div>
+      <ConverterContainer />
     </main>
   );
 };
---
+++ src/components/ConverterContainer/index.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const ConverterContainer = () => {
+  return <section></section>;
+};
+
+export default ConverterContainer;
