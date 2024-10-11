be5dece5faa25446657bfdd4c24c0b97c79d3849
tcocco20 <theo@coccoscoding.dev>
2024-10-11 11:59:25 -0700

    trying to figure out how to use react select

--- src/components/ConverterContainer/ConverterCurrencySelector.tsx
+++ src/components/ConverterContainer/ConverterCurrencySelector.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import { type SingleValue } from "react-select";
+import Select from "react-select/base";
+
 interface ConverterCurrencySelectorProps {
   isFromCurrency?: boolean;
 }
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
 const ConverterCurrencySelector = ({
   isFromCurrency,
 }: ConverterCurrencySelectorProps) => {
+  const [selectedCurrency, setSelectedCurrency] = useState(options[0]);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
   return (
     <div
       className={`w-full px-2 py-4 rounded-md${
@@ -15,7 +31,22 @@ const ConverterCurrencySelector = ({
       <p className="text-xs text-gray-300 font-light">
         {isFromCurrency ? "You sell" : "You buy"}
       </p>
-      <div className="flex justify-between border-b-black dark:border-b-white border-width-1"></div>
+      <div className="flex justify-between border-b-black dark:border-b-white border-width-1">
+        {options.length > 0 ? (
+          <Select
+            options={options}
+            inputValue={selectedCurrency.label}
+            onChange={(item: SingleValue<{ value: string; label: string }>) =>
+              setSelectedCurrency(item as { value: string; label: string })
+            }
+            value={selectedCurrency}
+            onInputChange={(inputValue) => inputValue}
+            onMenuOpen={() => setMenuIsOpen(true)}
+            onMenuClose={() => setMenuIsOpen(false)}
+            menuIsOpen={menuIsOpen}
+          />
+        ) : null}
+      </div>
     </div>
   );
 };
