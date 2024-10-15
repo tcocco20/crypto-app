88fb72715a64e6999d2b2fade49dff7cd3390c88
tcocco20 <theo@coccoscoding.dev>
2024-10-15 14:32:39 -0700

    added small fix for eslint

--- src/components/ConverterContainer/index.tsx
+++ src/components/ConverterContainer/index.tsx
@@ -10,6 +10,7 @@ const ConverterContainer = ({ coins }: ConverterContainerProps) => {
   return (
     <section className="w-full flex flex-col gap-4">
       <div className="flex flex-col gap-4">
+        <p>{coins.length}</p>
         <ConverterCurrencySelector isFromCurrency />
         <ConverterCurrencySelector />
       </div>
