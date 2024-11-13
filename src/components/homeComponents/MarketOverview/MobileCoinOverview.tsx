221bdf5be77d780161e594b2aa6857d9d104b96f
tcocco20 <theo@coccoscoding.dev>
2024-11-12 23:35:46 -0800

    Resolving conflict

--- src/components/homeComponents/MarketOverview/MobileCoinOverview.tsx
+++ src/components/homeComponents/MarketOverview/MobileCoinOverview.tsx
@@ -1,5 +1,6 @@
 import { type CoinData } from "@/actions/getCoinsList";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface MobileCoinOverviewProps {
@@ -15,7 +16,10 @@ const MobileCoinOverview = ({ coin }: MobileCoinOverviewProps) => {
     ? coin.price_change_percentage_24h.toFixed(2) + "%"
     : "No data";
   return (
-    <div className="flex gap-4 items-center bg-white dark:bg-violet-950/90 rounded-md p-3 text-black dark:text-white">
+    <Link
+      href={`/coin/${coin.id}`}
+      className="flex gap-4 items-center bg-white dark:bg-violet-950/90 rounded-md p-3 text-black dark:text-white"
+    >
       <Image
         src={coin.image}
         alt={"logo for " + coin.name}
@@ -40,7 +44,7 @@ const MobileCoinOverview = ({ coin }: MobileCoinOverviewProps) => {
           {displayPriceChange}
         </p>
       </div>
-    </div>
+    </Link>
   );
 };
 
