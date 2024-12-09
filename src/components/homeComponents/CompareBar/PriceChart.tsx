4eea1611689beac6c0cff0dd39305e2dc68ae68b
tcocco20 <theo@coccoscoding.dev>
2024-12-08 16:48:06 -0800

    Got the chart showing, now I need to get the correct data from API

--- src/components/homeComponents/CompareBar/PriceChart.tsx
+++ src/components/homeComponents/CompareBar/PriceChart.tsx
@@ -1,7 +1,16 @@
 import Card from "@/components/UI/Card";
 import React from "react";
-// import { Line } from "react-chartjs-2";
-// import { Chart as ChartJS } from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js/auto";
 
 interface PriceChartProps {
   title: string;
@@ -9,19 +18,29 @@ interface PriceChartProps {
   date: string;
 }
 
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 const PriceChart = ({ title, price, date }: PriceChartProps) => {
   return (
     <Card className="p-4 flex flex-col gap-2">
       <p className="text-sm text-gray-300">{title}</p>
       <p className="font-medium text-lg">${price}</p>
       <p className="text-xs text-gray-400">{date}</p>
-      {/* <Line
+      <Line
         data={{
           labels: ["1", "2", "3", "4", "5", "6", "7"],
           datasets: [
             {
               label: "Price",
-              data: [1, 2, 3, 4, 5, 6, 7],
+              data: [1, 3, 2, 7, 2, 4, 8],
               backgroundColor: "rgba(75,192,192,0.2)",
               borderColor: "rgba(75,192,192,1)",
             },
@@ -34,7 +53,7 @@ const PriceChart = ({ title, price, date }: PriceChartProps) => {
             },
           },
         }}
-      /> */}
+      />
     </Card>
   );
 };
