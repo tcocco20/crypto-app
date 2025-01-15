d464438aad0f697be73f43ff6e3bc4b6d43b5a56
tcocco20 <theo@coccoscoding.dev>
2025-01-14 16:35:20 -0800

    started creating a function to help with configuring fetch calls.

---
+++ src/utils/coingeckoFetch.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { composeUrl } from "./composeUrl";
+import { ExtractVariables } from "./types/ExtractVariables";
+
+export const coingeckoFetch = async <T>({
+  cache = "force-cache",
+  headers,
+  baseUrl,
+  variables,
+}: {
+  cache?: RequestCache;
+  headers?: HeadersInit;
+  baseUrl: string;
+  variables?: ExtractVariables<T>;
+}): Promise<{ status: number; body: T } | never> => {
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "x-cg-demo-api-key": process.env.API_SECRET_KEY,
+      ...headers,
+    } as HeadersInit,
+    cache,
+  };
+
+  const url = composeUrl(baseUrl, variables);
+
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error("Failed to connect to CoinGecko API");
+  }
+  return await response.json();
+};
