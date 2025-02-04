a51b62c9b449f571cce5ab05213581333c2a695e
tcocco20 <theo@coccoscoding.dev>
2025-02-04 11:38:36 -0800

    Mostly done with the coin selector buttons, might do charts as well since that was very simple. Might also bring in component from a different story.

--- src/components/homeComponents/CompareBar/CoinButton.tsx
+++ src/components/homeComponents/CompareBar/CoinButton.tsx
@@ -1,30 +1,42 @@
+"use client";
+
 import React from "react";
 import SelectableWrapper from "../../UI/SelectableWrapper";
 import Image from "next/image";
+import { type ListCoin } from "@/lib/types/ListCoin";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 interface CoinButtonProps {
   selected?: boolean;
-  name: string;
-  image: string;
+  coin: ListCoin;
   onClick?: () => void;
 }
 
-const CoinButton = ({
-  selected = false,
-  name,
-  image,
-  onClick,
-}: CoinButtonProps) => {
+const CoinButton = ({ selected = false, coin, onClick }: CoinButtonProps) => {
+  const displaySymbol = coin.symbol.toUpperCase();
+  const isMobile = useIsMobile();
+  const iconSize = isMobile ? 24 : 38;
   return (
     <SelectableWrapper selected={selected} widthClasses="w-fit">
       <button
         onClick={onClick}
-        className={`p-3 w-full rounded-md flex items-center gap-2 ${
+        className={`p-3 md:p-5 w-full rounded-md flex items-center gap-2 md:gap-4 ${
           !selected ? "bg-white dark:bg-violet-950" : ""
         }`}
       >
-        <Image src={image} alt={name} width={24} height={24} />
-        {name.toUpperCase()}
+        <Image
+          src={coin.image}
+          alt={coin.name}
+          width={iconSize}
+          height={iconSize}
+        />
+        <div className="flex flex-col items-start">
+          <p className="md:hidden">{displaySymbol} </p>
+          <p className="hidden md:block text-lg font-light">
+            {coin.name} ({displaySymbol})
+          </p>
+          <p className="font-light">{coin.current_price} USD ^ 15.6%</p>
+        </div>
       </button>
     </SelectableWrapper>
   );
