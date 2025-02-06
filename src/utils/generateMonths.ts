a1f10adc72f9c40f93a15eefcfdbe2964713057b
tcocco20 <theo@coccoscoding.dev>
2025-02-06 13:14:34 -0800

    Might have fixed months, will test later

--- src/utils/generateMonths.ts
+++ src/utils/generateMonths.ts
@@ -3,7 +3,7 @@ export const generateMonths = () => {
   const months = [];
   const currentMonth = today.getMonth();
   for (let i = 0; i < 12; i++) {
-    months.unshift((currentMonth + i) % 12);
+    months.unshift((currentMonth - i) % 12);
   }
   return months;
 };
