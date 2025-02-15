a4e0add12827036062a19e7bae31de32197abc30
tcocco20 <theo@coccoscoding.dev>
2025-02-15 13:47:40 -0800

    tried setting up initial coin render, will test out next

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -2,18 +2,28 @@
 import { useEffect, useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, type AppStore } from "../lib/store";
+import { loadCoins } from "@/lib/features/coinList/coinListSlice";
+import actions from "@/actions";
 
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>();
+  const storeRef = useRef<AppStore | null>(null);
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const loadInitialCoins = async () => {
+      if (storeRef.current) {
+        const coinList = await actions.getCoinsList("usd", 1, true);
+        storeRef.current.dispatch(loadCoins(coinList));
+      }
+    };
+    loadInitialCoins();
+  }, []);
 
   return <Provider store={storeRef.current}>{children}</Provider>;
 }
