8dfd9a8ea4e4711e3092e48983d20a78df69f252
tcocco20 <theo@coccoscoding.dev>
2025-02-15 12:07:32 -0800

    started working on setting up state for coins list, decided I will handle all coinsList data in redux, need to set up adding coins correctly and loading initial data as well.

---
+++ src/lib/features/coinData/coinDataSlice.ts
@@ -0,0 +1,27 @@
+import { type ListCoin } from "@/lib/types/ListCoin";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+export interface CoinListState {
+  coins: ListCoin[];
+  pagesLoaded: number;
+}
+
+const initialState: CoinListState = {
+  coins: [],
+  pagesLoaded: 0,
+};
+
+export const CoinListSlice = createSlice({
+  name: "coinData",
+  initialState,
+  reducers: {
+    loadCoins: (state, action: PayloadAction<ListCoin[]>) => {
+      state.coins = action.payload;
+      state.pagesLoaded += 1;
+    },
+  },
+});
+
+export const { loadCoins } = CoinListSlice.actions;
+
+export default CoinListSlice.reducer;
