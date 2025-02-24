5f9c726ac68adf6fe48d287164a2bf22094b68ab
tcocco20 <theo@coccoscoding.dev>
2025-02-24 13:01:27 -0800

    Search Results are showing now, but need to fix dropdown, backdrop is above all else in z index

--- src/components/DesktopHeader/SearchButton.tsx
+++ src/components/DesktopHeader/SearchButton.tsx
@@ -1,27 +1,64 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Dropdown from "../UI/Dropdown";
 import HeaderButton from "../UI/HeaderButton";
 import { Search } from "lucide-react";
+import actions from "@/actions";
+import { type SearchResult } from "@/lib/types/SearchResult";
+import Link from "next/link";
 
 const SearchButton = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
+
   const searchInputRef = useRef<HTMLInputElement>(null);
 
   const handleSearchClick = () => {
-    setDropdownOpen((prev) => !prev);
+    setDropdownOpen(true);
     if (searchInputRef.current) {
       searchInputRef.current.focus();
     }
   };
 
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    const getSearch = async () => {
+      const searchResults = await actions.getSearchResults(searchQuery);
+      setSearchResults(searchResults);
+    };
+
+    if (searchQuery.length > 0) {
+      timer = setTimeout(() => {
+        getSearch();
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const renderSearchResult = (item: SearchResult) => {
+    return (
+      <Link className="text-sm" href={`/coin/${item.id}`}>
+        {item.name}
+      </Link>
+    );
+  };
+
+  const extractKey = (item: SearchResult) => item.id;
+
   return (
     <Dropdown
       menuClassName="text-indigo-900 bg-indigo-600/15 dark:text-white dark:bg-violet-950 w-full border border-indigo-600/5 dark:border-gray-700/80 rounded-b"
-      parentClassName="h-full"
+      parentClassName="h-full z-10"
       containerClassName="h-full"
       onBackgroundClick={() => setDropdownOpen(false)}
+      data={searchResults}
+      renderItem={renderSearchResult}
+      keyExtractor={extractKey}
     >
       <HeaderButton
         className={`lg:px-4 w-[14.2rem] lg:w-72 xl:w-96 pointer-events-none active:opacity-100 h-full ${
@@ -29,12 +66,14 @@ const SearchButton = () => {
         }`}
         onClick={handleSearchClick}
       >
-        <Search size={18} strokeWidth={2.5} />
+        <Search size={18} strokeWidth={2.5} onClick={handleSearchClick} />
         <input
           type="text"
           placeholder="Search..."
           className="bg-transparent outline-none flex-1 pointer-events-auto"
           ref={searchInputRef}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
       </HeaderButton>
     </Dropdown>
--- src/components/UI/Dropdown.tsx
+++ src/components/UI/Dropdown.tsx
@@ -53,7 +53,7 @@ function Dropdown<T>({
       {isOpen && (
         <Portal>
           <div
-            className="absolute top-0 left-0 right-0 bottom-0 z-40"
+            className="absolute top-0 left-0 right-0 bottom-0 z-30"
             onClick={handleBackgroundClick}
           ></div>
         </Portal>
