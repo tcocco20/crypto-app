e92010c3f31a875194d942a46e2f9fc52cad46df
tcocco20 <theo@coccoscoding.dev>
2025-03-28 13:52:44 -0700

    added selected currency to price page, just need to fix getPriceDetails to display selected currency and make investment details show investment details if you have a coin you've invested

--- src/components/coinPageComponents/MobileLayout/index.tsx
+++ src/components/coinPageComponents/MobileLayout/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { IndividualCoin } from "@/lib/types/IndividualCoin";
 import React from "react";
 import CoinBrand from "@/components/UI/CoinBrand";
@@ -6,12 +8,17 @@ import CoinDescription from "../CoinDescription";
 import Card from "@/components/UI/Card";
 import MarketData from "../MarketData";
 import ExtraLinks from "../ExtraLinks";
+import { useAppSelector } from "@/lib/hooks";
 
 interface MobileLayoutProps {
   coin: IndividualCoin;
 }
 
 const MobileLayout = ({ coin }: MobileLayoutProps) => {
+  const selectedCurrency = useAppSelector(
+    (state) => state.preferences.selectedCurrency
+  ).toLowerCase();
+
   return (
     <div className="flex flex-col gap-2 md:hidden pt-8 pb-20 dark:text-white">
       <CoinBrand
@@ -20,11 +27,11 @@ const MobileLayout = ({ coin }: MobileLayoutProps) => {
         imageUrl={coin.image.small}
         className="py-4"
       />
-      <InvestmentDetails coin={coin} selectedCurrency="usd" />
+      <InvestmentDetails coin={coin} selectedCurrency={selectedCurrency} />
       <Card className="py-4 px-4">
         <CoinDescription description={coin.description} />
       </Card>
-      <MarketData coin={coin} />
+      <MarketData coin={coin} selectedCurrency={selectedCurrency} />
       <ExtraLinks
         homepage={coin.links.homepage}
         blockchainSite={coin.links.blockchain_site[0]}
