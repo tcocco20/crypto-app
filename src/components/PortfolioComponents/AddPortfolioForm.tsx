12faabc17777bf43c3c4a3b12a97a2b5ee75227e
tcocco20 <theo@coccoscoding.dev>
2025-03-29 17:26:34 -0700

    made add form work for edit as well, will test out next

--- src/components/PortfolioComponents/AddPortfolioForm.tsx
+++ src/components/PortfolioComponents/AddPortfolioForm.tsx
@@ -7,26 +7,41 @@ import { DialogClose } from "@radix-ui/react-dialog";
 import SelectableWrapper from "../UI/SelectableWrapper";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { type SearchResult } from "@/lib/types/SearchResult";
-import { addCoinToPortfolio } from "@/lib/features/portfolio/portfolioSlice";
+import {
+  addCoinToPortfolio,
+  editPortfolioCoin,
+  removeCoinById,
+} from "@/lib/features/portfolio/portfolioSlice";
 import { formatPortfolioCoinDate } from "@/utils/formatPortfolioCoinDate";
 import { getAmountPurchased } from "@/utils/getAmountPurchased";
 import actions from "@/actions";
 import { MarketDataArray } from "@/utils/types/MarketDataArray";
+import { PortfolioCoinWithMarketData } from "@/lib/types/PortfolioCoinWithMarketData";
 
-const AddPortfolioForm = () => {
-  const selectedCurrency = useAppSelector(
-    (state) => state.preferences.selectedCurrency
-  );
-  const dispatch = useAppDispatch();
-  const [selectedCoin, setSelectedCoin] = useState<SearchResult | null>(null);
-  const [amount, setAmount] = useState("");
-  const [formSubmitAttempted, setFormSubmitAttempted] = useState(false);
-  const [date, setDate] = useState("");
+interface AddPortfolioFormProps {
+  coinToEdit?: PortfolioCoinWithMarketData;
+}
 
+const AddPortfolioForm = ({ coinToEdit }: AddPortfolioFormProps) => {
   const today = new Date().toISOString().split("T")[0];
   const oneYearAgo = new Date(today);
   oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
   const minDate = oneYearAgo.toISOString().split("T")[0];
+  const selectedCurrency = useAppSelector(
+    (state) => state.preferences.selectedCurrency
+  );
+
+  const dispatch = useAppDispatch();
+  const [selectedCoin, setSelectedCoin] = useState<SearchResult | null>(null);
+  const [amount, setAmount] = useState(
+    coinToEdit?.amountPurchased[selectedCurrency] || ""
+  );
+  const [formSubmitAttempted, setFormSubmitAttempted] = useState(false);
+  const [date, setDate] = useState(
+    coinToEdit && new Date(coinToEdit.datePurchased) > new Date(minDate)
+      ? coinToEdit.datePurchased
+      : ""
+  );
 
   const handleItemSelect = (item?: SearchResult) => {
     setSelectedCoin(item!);
@@ -72,6 +87,73 @@ const AddPortfolioForm = () => {
     );
   };
 
+  const handleEditCoin = async (event: MouseEvent<HTMLButtonElement>) => {
+    setFormSubmitAttempted(true);
+    if (!date || invalidateAmount()) {
+      event.preventDefault();
+      return;
+    }
+
+    const datePurchased = formatPortfolioCoinDate(date);
+    let priceAtPurchase: MarketDataArray = {};
+
+    try {
+      if (selectedCoin) {
+        priceAtPurchase = await actions.getHistoricalDataForPortfolio(
+          selectedCoin.id,
+          datePurchased
+        );
+      } else {
+        priceAtPurchase = await actions.getHistoricalDataForPortfolio(
+          coinToEdit!.coinId,
+          datePurchased
+        );
+      }
+    } catch (error) {
+      alert(`Error adding coin to portfolio: ${error}`);
+    }
+
+    const amountPurchased = getAmountPurchased(
+      priceAtPurchase,
+      +amount,
+      selectedCurrency
+    );
+
+    if (selectedCoin) {
+      const { id, name, symbol, image } = selectedCoin;
+
+      dispatch(
+        editPortfolioCoin({
+          id: coinToEdit!.id,
+          coinId: id,
+          name,
+          symbol,
+          image,
+          datePurchased,
+          priceAtPurchase,
+          amountPurchased,
+        })
+      );
+    } else {
+      dispatch(
+        editPortfolioCoin({
+          id: coinToEdit!.id,
+          coinId: coinToEdit!.coinId,
+          name: coinToEdit!.name,
+          symbol: coinToEdit!.symbol,
+          image: coinToEdit!.image,
+          datePurchased,
+          priceAtPurchase,
+          amountPurchased,
+        })
+      );
+    }
+  };
+
+  const handleDeleteCoin = () => {
+    dispatch(removeCoinById(coinToEdit!.id));
+  };
+
   const invalidateAmount = () => {
     return formSubmitAttempted && (!amount || +amount <= 0);
   };
@@ -84,16 +166,51 @@ const AddPortfolioForm = () => {
     setDate(e.target.value);
   };
 
-  return (
-    <div className="grid grid-cols-5 gap-4">
-      {selectedCoin && (
+  const cancelButton = (
+    <DialogClose asChild>
+      <button className="flex-1 py-2 text-center w-full bg-indigo-600/15 dark:bg-violet-900/50 rounded disabled:opacity-50">
+        Cancel
+      </button>
+    </DialogClose>
+  );
+
+  const deleteButton = (
+    <DialogClose asChild>
+      <button
+        className="flex-1 py-2 text-center w-full bg-red-700 rounded disabled:opacity-50"
+        onClick={handleDeleteCoin}
+      >
+        Delete Coin
+      </button>
+    </DialogClose>
+  );
+
+  const generateCoinBranding = () => {
+    if (selectedCoin) {
+      return (
         <CoinBrand
           className="col-span-2"
           name={selectedCoin.name}
           symbol={selectedCoin.symbol}
           imageUrl={selectedCoin.image}
         />
-      )}
+      );
+    } else if (coinToEdit) {
+      return (
+        <CoinBrand
+          className="col-span-2"
+          name={coinToEdit.name}
+          symbol={coinToEdit.symbol}
+          imageUrl={coinToEdit.image}
+        />
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div className="grid grid-cols-5 gap-4">
+      {generateCoinBranding()}
       <div className="space-y-4 col-span-3 col-start-3">
         <DesktopSearchComponent
           onItemSelect={handleItemSelect}
@@ -131,11 +248,7 @@ const AddPortfolioForm = () => {
           errorText="Please enter the date you purchased the coin."
         />
         <div className="flex items-center gap-2 text-center">
-          <DialogClose asChild>
-            <button className="flex-1 py-2 text-center w-full dark:bg-violet-900/50 rounded disabled:opacity-50">
-              Cancel
-            </button>
-          </DialogClose>
+          {coinToEdit ? deleteButton : cancelButton}
           <div
             className={`flex-1 ${
               (!selectedCoin ||
@@ -147,7 +260,7 @@ const AddPortfolioForm = () => {
               <DialogClose asChild>
                 <button
                   className="p-2 text-center w-full"
-                  onClick={handleAddCoin}
+                  onClick={coinToEdit ? handleEditCoin : handleAddCoin}
                 >
                   Save Currency
                 </button>
