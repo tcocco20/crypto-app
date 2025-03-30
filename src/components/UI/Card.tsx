55b66bd1a489a325257acda7afa27cbdc0df2dd3
tcocco20 <theo@coccoscoding.dev>
2025-03-29 18:23:04 -0700

    did not need to add onClick for card

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -3,17 +3,12 @@ import { type ReactNode } from "react";
 const Card = ({
   children,
   className,
-  onClick,
 }: {
   className?: string;
   children: ReactNode;
-  onClick?: () => void;
 }) => {
   return (
-    <div
-      className={`bg-white dark:bg-indigo-900/40 rounded-md ${className}`}
-      onClick={onClick}
-    >
+    <div className={`bg-white dark:bg-indigo-900/40 rounded-md ${className}`}>
       {children}
     </div>
   );
