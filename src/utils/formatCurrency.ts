f66bbaa38654677dfdf6970f6947175c3a849242
tcocco20 <theo@coccoscoding.dev>
2025-03-31 19:04:11 -0700

    function is ready for testing, will test next and try to use it in most spots

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,4 +1,30 @@
-export const formatCurrency = (currencyValue: number) => {
-  const number = new Intl.NumberFormat().format(currencyValue);
-  return number;
+const isCurrencySupported = (currencyCode: string): boolean => {
+  try {
+    new Intl.NumberFormat(undefined, {
+      style: "currency",
+      currency: currencyCode,
+    });
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+export const formatCurrency = (
+  currencyValue: number,
+  currencyDisplay = false,
+  selectedCurrency = ""
+) => {
+  const userLocale = navigator.language || "en-US";
+
+  if (!currencyDisplay)
+    return new Intl.NumberFormat(userLocale).format(currencyValue);
+  if (!selectedCurrency || !isCurrencySupported(selectedCurrency))
+    return new Intl.NumberFormat(userLocale, { style: "currency" }).format(
+      currencyValue
+    );
+  return new Intl.NumberFormat(userLocale, {
+    style: "currency",
+    currency: selectedCurrency,
+  }).format(currencyValue);
 };
